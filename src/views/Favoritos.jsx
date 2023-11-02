72ec610c16fd240eaf313c069752743e7dd715c0
GabuGabriel <gabriel.munoz.solis@gmail.com>
2023-11-01 21:22:28 -0300

    ultimosarreglos

--- src/views/Favoritos.jsx
+++ src/views/Favoritos.jsx
@@ -7,22 +7,24 @@ export default function Favoritos() {
     const favoritos = imagenes.filter((imagen) => imagen.liked);
 
     return (
-        <div className="containerFavoritas">
-            <h1>Fotos favoritas</h1>
-            <div class="container-fluid overflow-hidden text-center">
-                <div class="row gy-5 d-flex align-items-center">
-                    {favoritos.map((imagen) => (
-                        <div class="col">
-                            <div class="card" style={{ width: "18rem", height: "18rem" }} key={imagen.id} >
-                                <img class="card-img-top" src={imagen.src.tiny} alt={imagen.alt} />
-                                <div class="card-body ">
-                                    <h5 class="card-title">{imagen.photographer} </h5>
+        <>
+            <div className="containerFavoritas">
+                <h1>Fotos favoritas</h1>
+                <div class="container overflow-hidden text-center">
+                    <div class="row gy-5">
+                        {favoritos.map((imagen) => (
+                            <div class="col">
+                                <div class="card" style={{ width: "18rem", height: "18rem" }} key={imagen.id} >
+                                    <img class="card-img-top" src={imagen.src.tiny} alt={imagen.alt} />
+                                    <div class="card-body ">
+                                        <h5 class="card-title">{imagen.photographer} </h5>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
