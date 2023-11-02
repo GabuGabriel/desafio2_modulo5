58cb110f989e841a19d34e513781206e8697877b
GabuGabriel <gabriel.munoz.solis@gmail.com>
2023-11-01 21:07:51 -0300

    funcionalidadcompletada

--- src/views/Favoritos.jsx
+++ src/views/Favoritos.jsx
@@ -1,21 +1,28 @@
+import '../App.css'
 import React from "react";
 import { useImagenes } from "../components/Contexto/ImagenesContext";
 
 export default function Favoritos() {
-  const { imagenes } = useImagenes();
+    const { imagenes } = useImagenes();
+    const favoritos = imagenes.filter((imagen) => imagen.liked);
 
-  const favoritos = imagenes.filter((imagen) => imagen.liked);
-
-  return (
-    <div className="containerFavoritas">
-      <h1>Fotos favoritas</h1>
-      <div className="galeria grid-columns-5 p-3">
-        {favoritos.map((imagen) => (
-          <div key={imagen.id}>
-            <img src={imagen.url} alt={imagen.alt} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="containerFavoritas">
+            <h1>Fotos favoritas</h1>
+            <div class="container-fluid overflow-hidden text-center">
+                <div class="row gy-5 d-flex align-items-center">
+                    {favoritos.map((imagen) => (
+                        <div class="col">
+                            <div class="card" style={{ width: "18rem", height: "18rem" }} key={imagen.id} >
+                                <img class="card-img-top" src={imagen.src.tiny} alt={imagen.alt} />
+                                <div class="card-body ">
+                                    <h5 class="card-title">{imagen.photographer} </h5>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
